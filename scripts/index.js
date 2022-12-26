25a39ecde73c5f0ca12a59cb2a3321854ec0e9b5
Deryabin Aleksey <deryadinalex@bk.ru>
2022-12-26 22:47:41 +0300

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -4,20 +4,20 @@ let closepopUp = document.querySelector(".popup__close");
 
 let nameUser = document.querySelector(".profile__name");
 let profession = document.querySelector(".profile__text");
-let formElement = document.querySelector(".popup__container");
+let formElement = document.querySelector(".popup__input");
 let nameInput = document.querySelector(".popup__input-save_type_name");
 let jobInput = document.querySelector(".popup__input-save_type_about");
 
 openPopUp.addEventListener("click", handleOpen);
 function handleOpen() {
-  popUp.classList.add("popup__display_open");
+  popUp.classList.add("popup_display-open");
   nameInput.value = nameUser.textContent;
   jobInput.value = profession.textContent;
 }
 
 closepopUp.addEventListener("click", handleClose);
 function handleClose() {
-  popUp.classList.remove("popup__display_open");
+  popUp.classList.remove("popup_display-open");
 }
 
 function handleFormSubmit(evt) {
